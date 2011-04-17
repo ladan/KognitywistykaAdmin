3a851a1b3c0c16593f5ee9d529831e5b5829c12c
Damian <damikl1989+github@gmail.com>
2011-04-17 22:12:58 +0200

    Poprawiona walidacja linków, teraz korzysta z plików messages oraz lepiej rozpoznaje protokół

--- src/main/java/pl/edu/uj/kognitywistyka/admin/util/UrlConverter.java
+++ src/main/java/pl/edu/uj/kognitywistyka/admin/util/UrlConverter.java
@@ -2,13 +2,18 @@ package pl.edu.uj.kognitywistyka.admin.util;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
+
+import org.apache.commons.validator.UrlValidator;
 
 @FacesConverter(forClass=URL.class)
 public class UrlConverter implements Converter {
@@ -16,12 +21,38 @@ public class UrlConverter implements Converter {
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
         try {
-        	if(!value.contains("http://"))
+        	if(!value.contains("://"))
         		value = "http://" + value;
-            return new URL(value);
+            URL address = new URL(value);
+            
+    		UrlValidator urlValidator = new UrlValidator();
+    		
+    		if(!urlValidator.isValid(address.toString())){
+     
+    		/*	FacesMessage msg = 
+    				new FacesMessage("URL Conversion error.", 
+    						"Invalid URL format.");
+    		*/
+    	
+    			throw new ConverterException(getErrorMessage(context,"UrlConversionError"));
+    		}
+    		return address;
+            
         } catch (MalformedURLException e) {
-            throw new ConverterException(new FacesMessage(String.format("Cannot convert %s to URL", value)), e);
+            throw new ConverterException(getErrorMessage(context,"UrlUnknownProtocol"), e);
+            // new FacesMessage(String.format("Cannot convert %s to URL", value))
         }
+       
+    }
+    
+    private FacesMessage getErrorMessage(FacesContext context, String resourceid)
+    {
+    	UIViewRoot viewRoot = context.getViewRoot();
+    	Locale locale = viewRoot.getLocale();
+    	ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    	ResourceBundle bundle = ResourceBundle.getBundle("messages", locale, loader);
+    	String resource = bundle.getString(resourceid);
+    	return new FacesMessage(resource);
     }
 
 	@Override
